6f36f7365349ada22a49ab26c5abd91128aaa00a
Sim Shang En <simshangen@gmail.com>
2025-05-27 13:33:42 +0100

    Update promotion script

--- data_models/promote.sh
+++ data_models/promote.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 # This script promotes the generated JSON schemas to the schemas directory.
 
-rm -r ../schemas/*
-cp tsp-output/@typespec/json-schema/*.yaml ../schemas/
+rm -rf ../schemas/*
+cp tsp-output/@typespec/json-schema/*.yaml ../schemas
